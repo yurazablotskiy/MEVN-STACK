87af85d8f0dbbe4b01ff52b6f29e6a55aa5def99
Yura Zablotskiy <110226938+yurazablotskiy@users.noreply.github.com>
2023-11-29 12:46:17 +0200

    Update app.js

--- Articles-site/server/app.js
+++ Articles-site/server/app.js
@@ -7,27 +7,14 @@ import articleRouter from './routes/articleRoutes.js';
 import commentRouter from './routes/commentRoutes.js';
 
 const app = express();
-app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
-app.use(cors({
-  origin: ["https://articles-site-client.vercel.app"],
-  methods: ["POST","GET"],
-  credentials: true
-}));
-
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 connect('mongodb+srv://dbYura:x90KglAoIfTKjW1x@cluster0.wq8ibv3.mongodb.net/blog?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
-  .then(() => {
-    console.log('Вы подключились к БД');
-  })
   .catch((err) => {
     throw err;
   });
@@ -35,6 +22,6 @@ connect('mongodb+srv://dbYura:x90KglAoIfTKjW1x@cluster0.wq8ibv3.mongodb.net/blog
 app.use('/', articleRouter);
 app.use('/', commentRouter);
 
-app.listen(3000, () =>
-  console.log('Сервер запущен по адресу http://localhost:3000')
+app.listen(PORT, () =>
+  console.log(`Server is running on port ${PORT}`)
 );
