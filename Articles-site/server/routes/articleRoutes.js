894b3e677303c03bf5d35ab5396b8df1af267f32
Yura Zablotskiy <110226938+yurazablotskiy@users.noreply.github.com>
2023-11-29 18:55:35 +0200

    Update articleRoutes.js

--- Articles-site/server/routes/articleRoutes.js
+++ Articles-site/server/routes/articleRoutes.js
@@ -3,7 +3,7 @@ import { Articles } from '../models/article.js';
 
 const articleRouter = express.Router();
 
-articleRouter.get('/api/get-articles', async (req, res) => {
+articleRouter.get('/get-articles', async (req, res) => {
   try {
     const getArticles = await Articles.find().sort({ _id: 1 });
     if (!getArticles) throw new Error('Статьи не найдены');
