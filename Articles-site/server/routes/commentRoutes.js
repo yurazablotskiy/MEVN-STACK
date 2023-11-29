7f5f4bad349e8e0c21472d2138f1dbe5d06ae268
Yura Zablotskiy <110226938+yurazablotskiy@users.noreply.github.com>
2023-11-29 14:42:13 +0200

    Add files via upload

---
+++ Articles-site/server/routes/commentRoutes.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import { Comments } from '../models/comment.js';
+
+const commentRouter = express.Router();
+
+commentRouter.post('/add-post', async (req, res) => {
+  try {
+    const newAddPost = new Comments(req.query);
+    const result = await newAddPost.save();
+    if (!result) throw new Error('Мы не можем добваить комментарий');
+    res.status(200).send('Ok');
+  } catch (err) {
+    console.log(err);
+    res.status(500);
+  }
+});
+
+commentRouter.get('/get-posts', async (req, res) => {
+  try {
+    const getPosts = await Comments.find().sort({ _id: -1 });
+    if (!getPosts) throw new Error('Статьи не найдены');
+    res.status(200).send(getPosts);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
+
+export default commentRouter;
