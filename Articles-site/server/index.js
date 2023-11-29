bec11623d1167ac32540c0040b1b9c26570130c9
Yura Zablotskiy <110226938+yurazablotskiy@users.noreply.github.com>
2023-11-29 15:01:44 +0200

    Update index.js

--- Articles-site/server/index.js
+++ Articles-site/server/index.js
@@ -8,7 +8,12 @@ import commentRouter from './routes/commentRoutes.js';
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors(
+  origin: ["https://article-site-server.vercel.app
+"],
+  methods: ["POST","GET"],
+  credentials: true
+));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
